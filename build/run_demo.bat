6e71c2eb4100eaa1bf1fd9bb5e508285b458e098
Oliver Tearne <tearne@gmail.com>
2011-02-07 18:33:53 +0000

    Fix font issues for WinXP, add num test req to pdf.  Release 1.4

--- build/run_demo.bat
+++ build/run_demo.bat
@@ -1,2 +1,2 @@
-call scala -classpath beaner_2.8.1-1.1.jar demo.scala %*
+call scala -classpath "lib/beaner_2.8.1-1.4.jar;lib/core.jar;lib/itext.jar;lib/pdf.jar;lib/commons-math-2.1.jar" script.txt
 PAUSE
