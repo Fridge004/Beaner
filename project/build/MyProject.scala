f558c494538e7ca0e254f34aa2a094bb1df69e11
Oliver Tearne <tearne@gmail.com>
2011-05-24 21:13:05 +0100

    Update to scala 2.9

--- project/build/MyProject.scala
+++ project/build/MyProject.scala
@@ -17,6 +17,6 @@ class MyProject(info: ProjectInfo) extends DefaultProject(info)
 
 
   lazy val junit = "junit" % "junit" % "4.5" % "test->default"
-  lazy val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  lazy val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.4.1" % "test"
   lazy val mockito = "org.mockito" % "mockito-all" % "1.8.5" %"test"
 }
--- project/plugins/MyPlugins.scala
+++ project/plugins/MyPlugins.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class MyPlugins(info: ProjectInfo) extends PluginDefinition(info){
-  lazy val eclipsify = "de.element34" % "sbt-eclipsify" % "0.6.0"
+  lazy val eclipsify = "de.element34" % "sbt-eclipsify" % "0.7.0"
   lazy val sbtIdeaRepo = "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
   lazy val sbtIdea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.2.0"
 }
